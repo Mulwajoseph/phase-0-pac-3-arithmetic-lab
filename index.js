d7ff824b7583e79e6fa449dfe91c4f4d848b4d01
mulwajoseph <mulwajose.jm@gmail.com>
2023-07-05 13:57:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(){
+    
+}
+function subtract(){
+
+}
+function multiply(){
+    
+}
+function divide(){
+    
+}
+
+function add(a,b){
+    return a+b;
+}
+console.log(add)
+
+function subtract(a,b){
+    return a-b;
+}
+console.log(subtract)
+
+function multiply(a,b){
+    return a*b
+}
+console.log(multiply)
+
+function divide(a,b){
+    return(a/b)
+}
+console.log(divide)
+
+function increment(n){
+    return(++n)
+}
+console.log(increment)
+
+
+function decrement(n){
+    return(--n)
+}
+console.log(decrement)
+
+function makeInt(n){
+    return(parseInt(n))
+}
+console.log(makeInt)
+console.log(makeInt("10"));
+console.log(makeInt("abc")); 
+
+function preserveDecimal(n){
+    return(parseFloat(n))
+}
+console.log(preserveDecimal)
